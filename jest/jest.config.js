3437e4fed03d4e076c2175eb65e51dc37b4f3659
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-05-29 15:29:41 +0300

    fix: переименованы файлы моков для jest

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -4,10 +4,11 @@ module.exports = ({ setupFilesAfterEnv }) => ({
   verbose: true,
   setupFilesAfterEnv: [setupFilesAfterEnv],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(__dirname, './__mocks__/fileMock.js'),
-    '\\.(css|less)$': path.join(__dirname, './__mocks__/styleMock.js'),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(__dirname, './mocks/file-mock.js'),
+    '\\.(css|less)$': path.join(__dirname, './mocks/style-mock.js'),
   },
-  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
   testMatch: ['**/*.test.{ts,tsx}'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
   collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.stories.tsx'],
 });
