b1dae338a3e3a68c39de7ff2196a52a3294e0ba0
Davs107 <davs777t@gmail.com>
2020-07-29 14:54:47 +0300

    chore(pretier): изменен импорт prettier
    Jira issue: VEGA-277

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   printWidth: 100,
   singleQuote: true,
-  quoteProps: "consistent",
-  trailingComma: "all",
-  arrowParens: "always",
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+  arrowParens: 'always',
   semi: true,
   tabWidth: 2,
   bracketSpacing: true,
