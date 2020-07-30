d65a52088b0fd26ee262ade62b6088cf387c8e29
Sergey Gavshin <inzephirum@gmail.com>
2020-07-31 01:06:41 +0700

    fix(lint-staged): использовать пользовательский lint-staged
    если у пользователя создан свой lint-staged.config.js, то мы используем его, иначе используем конфиг
    по умолчанию

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,4 +1,5 @@
-const joinPath = require("path.join");
+const fs = require('fs');
+const path = require('path');
 
 /* Пример использования:
 
@@ -12,17 +13,29 @@ module.exports = {
 
 */
 
+function join(...args) {
+  let fullPath = path.join(...args);
+
+  if (process.platform === 'win32') {
+    fullPath = fullPath.replace(/\\/g, '/');
+  }
+
+  return fullPath;
+}
+
+const defaultLintStagedConfigPath = join(__dirname, 'git', 'lint-staged.config.js');
+
+let lintStagedConfigPath = defaultLintStagedConfigPath;
+
+const userLintStagedConfigPath = join(process.cwd(), 'lint-staged.config.js');
+
+if (fs.existsSync(userLintStagedConfigPath)) {
+  lintStagedConfigPath = userLintStagedConfigPath;
+}
+
 module.exports = {
   hooks: {
-    "pre-commit": `lint-staged -c ${joinPath(
-      __dirname,
-      "git",
-      "lint-staged.config.js"
-    )}`,
-    "prepare-commit-msg": `${joinPath(
-      __dirname,
-      "git",
-      "prepare-commit-msg.sh"
-    )} $HUSKY_GIT_PARAMS`,
+    'pre-commit': `lint-staged -c ${lintStagedConfigPath}`,
+    'prepare-commit-msg': `${join(__dirname, 'git', 'prepare-commit-msg.sh')} $HUSKY_GIT_PARAMS`,
   },
 };
