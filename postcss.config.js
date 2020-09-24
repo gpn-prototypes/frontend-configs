77f87c51155480cf6ec4163a68a04e55fa1a559f
Oleg Denisov <c1n1kk@gmail.com>
2020-09-24 19:28:47 +0300

    fix(postcss-config): удалить поле

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,6 @@ const nested = require('postcss-nested');
 module.exports = function createPostcssConfig() {
   return {
     modules: false,
-    postcssOptions: {
-      plugins: [nested()],
-    },
+    plugins: [nested()],
   };
 };
